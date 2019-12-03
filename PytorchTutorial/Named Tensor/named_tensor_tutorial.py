aec65facd4088aee6f4cae56915e1b6073faa2a4
Nguyen Van Thanh <thanhnv@deha-soft.com>
2019-12-03 19:34:54 +0700

    add listing6-1.py

--- PytorchTutorial/Named Tensor/named_tensor_tutorial.py
+++
@@ -1,9 +0,0 @@
-# https://pytorch.org/tutorials/intermediate/named_tensor_tutorial.html
-# This is to create tensor with anmed dimensions as well as remove or rename those dimensions
-# Understand the basic of how operation propagate dimension names
-# See how nameing dimentsion enables clear code to :
-#  + broadcasting operations
-#  + flattening and unflattening dimension
-
-# Construct tensor with name
-import torch
