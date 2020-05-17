73d7aec04517b30ff4031be607d68141ffd9780b
NguyenVanThanh <NguyenVanThanhHust@gmail.com>
2020-05-17 15:46:49 +0700

    protocol buffer example

---
+++ gRPC/ReadMe.md
@@ -0,0 +1,1 @@
+https://www.semantics3.com/blog/a-simplified-guide-to-grpc-in-python-6c4e25f0c506/
---
+++ gRPC/calculator.py
@@ -0,0 +1,8 @@
+import math
+
+def square_root(x):
+    if x>0:
+        y = math.sqrt(x)
+    else:
+        return None
+    return y
