93c7b9025d721e65795c15b44bf7588895461a0d
Nguyen Van Thanh <38394613+NguyenVanThanhHust@users.noreply.github.com>
2022-10-18 13:36:41 +0700

    Step 1

---
+++ RealPython/Blog_Django_Vue_GraphQL/blog/schema.py
@@ -0,0 +1,6 @@
+from django.contrib.auth import get_user_model
+from graphene_django import DjangoObjectType
+
+from blog import models
+
+# Creat a corresponding class for each model and user model
---
+++ RealPython/Blog_Django_Vue_GraphQL/blog/schema/schema.py
@@ -0,0 +1,3 @@
+GRAPHENE = {
+  "SCHEMA": "blog.schema.schema",
+}
