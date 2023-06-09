f8400bf4002f6e5809ee967485b1bbf27cfeb460
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-06-09 16:25:50 +0700

    update download video youtube

--- download_video.py
+++ download_video.py
@@ -1,10 +1,16 @@
-import pytube
-video_urls = ['https://www.youtube.com/watch?v=KFvb_mluq8Y',
-		'https://www.youtube.com/watch?v=w-9i3hxmBso', 
-'https://www.youtube.com/watch?v=xRW_t7XAenk', 
-'https://www.youtube.com/watch?v=StGm-m1EUgk', 
-'https://www.youtube.com/watch?v=8vWaawiUteM']
-for video_url in video_urls:
-	youtube = pytube.YouTube(video_url)
-	video = youtube.streams.first()
-	video.download('../') # path, where to video download.
+import os
+from pytube import YouTube
+
+# Replace the video link with your desired link
+video_link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Create a YouTube object and get the highest resolution stream
+yt = YouTube(video_link)
+stream = yt.streams.get_highest_resolution()
+
+# Replace the file path with your desired file path
+output_folder = "./outputs/"
+
+os.makedirs(output_folder)
+# Download the video to the specified file path
+stream.download(output_folder)
